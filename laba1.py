6c20e770aa7cdd5ee968175dd9b97db1ee03f3e8
MikkeL0730 <57572232+MikkeL0730@users.noreply.github.com>
2023-02-10 16:17:32 +0400

    Add files via upload

--- laba1.py
+++ laba1.py
@@ -1,38 +1,32 @@
-file = open("test.txt", "r")
+def multiple_replace(target_str, replace_values):
+    for i, j in replace_values.items():
+        target_str = target_str.replace(i, j)
+    return target_str
+file = open("test2.txt", "r")
 k = int(input('Введи количество цифр'))
 kol_int = 0
-yes_int = 1
+yes_int = 0
 last_int = ''
 last_int_max = int(0)
-#print('Натуральные числа, у которых более', k, 'цифр:')
-while 1:
+char = 'test'
+print('Натуральные числа, у которых более', k, 'цифр:')
+while (char != ""):
     char = file.read(1)
-    if not char:
-        break
-    isInt = char.isdigit()
-    if (isInt == True):
-        yes_int = 1
+    if(char != " " and char != ""):
         char = str(char)
         last_int = last_int + char
     else:
-        if (yes_int == 1):
+        if(last_int.isdigit()):
             kol = len(last_int)
             if (kol > k):
                 kol_int += 1
                 int_temp = int(last_int)
                 if (int_temp > last_int_max):
                     last_int_max = int_temp
-                #print(last_int)
+                print(last_int)
         last_int = ''
-        yes_int = 0
 file.close()
-print('Всего чисел, содержащих более',k,'цифр -',kol_int)
-print('\nМаксимальное число -', last_int_max)
-last_temp = str(last_int_max)
-chisla = ['ноль ', 'один ', 'два ', 'три ', 'четыре ', 'пять', 'шесть ', 'семь ', 'восемь ', 'девять ']
-forr = 0
-for number in chisla:
-    forr2 = str(forr)
-    last_temp = last_temp.replace(forr2, number)
-    forr += 1
-print(last_temp)
+print('Всего чисел, содержащих более',k,'цифр -',kol_int,'\n','\nМаксимальное число -', last_int_max)
+#print()
+slovar = {"0": "ноль ", "1": "один ", "2": "два ", "3": "три ", "4": "четыре ", "5": "пять ", "6": "шесть ", "7": "семь ", "8": "восемь ", "9": "девять "}
+print(multiple_replace(str(last_int_max), slovar))
